cbfa440ff31e1b1a58603aa2ce7437bf69fe3c68
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2024-03-28 11:29:55 -0300

    Update Estrutura de Decisão.js

--- Dw2/Estrutura de Decisão.js
+++ Dw2/Estrutura de Decisão.js
@@ -1,1 +1,10 @@
+let num1 = Number(prompt("Digite o primeiro numero:"));
+let num2 = Number(prompt("Digite o segundo numero:"));
 
+if (num1 > num2){
+    console.log("O maior número é " + num1);
+} else if (num1 < num2) {
+    console.log("O maior número é " + num2);
+} else{
+    console.log("Os números são iguais.");
+}
