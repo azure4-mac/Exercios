57c69de5881ff0ebb0b1ad2d561e6788ed11fd88
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2024-04-04 07:57:04 -0300

    Atualizar o Estrutura De Repetição.js

--- Dw2/Estrutura De Repetição.js
+++ Dw2/Estrutura De Repetição.js
@@ -1,13 +1,12 @@
 /* 1 */
-let note = prompt("Please enter a number between 0 and 10:");
+let note = prompt("Digite um número entre 1 a 10:");
 
-// Check if the input is valid
 while (note < 0 || note > 10) {
-  // If not, show an error message and ask again
-  alert("Invalid value! Please enter a number between 0 and 10.");
-  note = prompt("Please enter a number between 0 and 10:");
+ 
+  alert("Valor inválido");
+  note = prompt("Digite um número entre 1 a 10");
 }
 
-// If the input is valid, show a success message
-alert("Valid value! You entered: " + note);
+alert("Valor válido! Número : " + note);
+
 
