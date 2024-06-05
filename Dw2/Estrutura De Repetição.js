26398ad7c9e7b4ff08528a891d7bdce21a1bf943
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2024-06-05 14:32:42 -0300

    Update Estrutura De Repetição.js

--- Dw2/Estrutura De Repetição.js
+++ Dw2/Estrutura De Repetição.js
@@ -1,12 +1,3 @@
-/* senha (2) */ 
-let senha = prompt("Digite uma senha:");
-
-while (senha&& senha.length < 6){
-    alert ('Deve possuir mais de 6 caracteres.')
-    senha = prompt('Digite uma senha')
-}
-alert ("Senha cadastrada com sucesso!")
-
 /* 1 */
 let note = prompt("Digite um número entre 1 a 10:");
 
@@ -17,6 +8,16 @@ while (note < 0 || note > 10) {
 }
 
 alert("Valor válido! Número : " + note);
+
+/* senha (2) */ 
+let senha = prompt("Digite uma senha:");
+
+while (senha&& senha.length < 6){
+    alert ('Deve possuir mais de 6 caracteres.')
+    senha = prompt('Digite uma senha')
+}
+alert ("Senha cadastrada com sucesso!")
+
 
 /* 3 */
 let nome, idade, salario, estado_civil;
