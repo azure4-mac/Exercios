9eb67e59dd1fbf65d7b1f974f67b3d344cbe92bb
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2024-06-05 15:06:23 -0300

    Update and rename Funções to Funções.js

---
+++ Dw2/Funções.js
@@ -0,0 +1,10 @@
+/* 1 */ 
+function sim (){
+    for (let x = 1; é <= n; x++){
+        let s =""
+        for (let y = 0; y < x; y++) {
+            s = s + " " + y;
+        }
+        console.log(s)
+    }
+}
