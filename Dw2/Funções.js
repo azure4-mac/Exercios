196953bab91dd9438f0baee975d4d6454b3cf82b
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2024-06-25 18:48:31 -0300

    Update Funções.js

--- Dw2/Funções.js
+++ Dw2/Funções.js
@@ -8,3 +8,35 @@ function sim (){
         console.log(s)
     }
 }
+
+/* 2 */ 
+let a1 = Number(prompt())
+let a2 = Number(prompt())
+let a3 = Number(prompt())
+
+function soma(a1, a2, a3){
+  return a1 + a2 + a3
+}
+
+/* 4 */
+let a1 = Number(prompt())
+let a2 = Number(prompt())
+
+function pn (a1, a2){
+  if (a1 < 0){
+    console.log(`%{a1} é um numero negativo`)
+  }
+  else if (a1 > 0){
+    console.log(`${a1} é um numero positivo`)
+  }
+  else if (a2 < 0 ){
+    console.log(`%{a2} é um numero negativo`)
+  }
+  else {
+    console.log(`${a2} é um numero positivo`)
+  }
+
+}
+pn(a1, a2)
+
+/* 5 */
